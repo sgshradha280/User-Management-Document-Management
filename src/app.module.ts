09d33514340b627639e148f52339996201ed5844
Gupta <oeapf61@HO06LAPTP13527.local>
2025-04-07 18:23:49 +0530

    updated app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { Document } from './documents/document.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'mydb.abc123.us-east-1.rds.amazonaws.com',
+      host: 'mydb.cluster-cj4660e4wc1r.eu-north-1.rds.amazonaws.com',
       port: 5432,
       username: 'postgres',
       password: 'postgres',
